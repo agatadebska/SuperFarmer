a28d1f51961b8a48158a88e405d6ca03d32612da
agatadebska <agata.martyna.debska@gmail.com>
2014-04-27 13:08:43 +0200

    Zmienia sie tlo w playPanel

--- src/resources/Panel.java
+++ src/resources/Panel.java
@@ -15,34 +15,32 @@ import javax.swing.JPanel;
 
 
 public class Panel extends JPanel{
-	private BufferedImage tlo;
-	private Integer height = new Integer(600);
-	private Integer width = new Integer(800);
+//	protected static BufferedImage tlo;
+	protected Integer height = new Integer(600);
+	protected Integer width = new Integer(800);
 	Panel(){
 		super();
 		setBounds(0,0, width, height);
-		try {
-			tlo = ImageIO.read(new File("src/graphics/tlo_new.jpg"));
-		} catch (IOException e) {
-			System.err.println("Blad odczytu obrazka");
-			e.printStackTrace();
-		}		
+			
 	}
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.drawImage(tlo, 0, 0, width, height, null);
+		//g.drawImage(tlo, 0, 0, width, height, null);
 	}
 }
 
 
 class MainPanel extends Panel{
-	
-	//private static JLayeredPane lpane;
-	//private static PlayPanel ppanel;	
-	
-	
+
+	private BufferedImage tlo;
 	MainPanel(final JLayeredPane lp, final PlayPanel pp){
 		//lpane = lp;
+		try {
+			tlo = ImageIO.read(new File("src/graphics/tlo_new.jpg"));
+		} catch (IOException e) {
+			System.err.println("Blad odczytu obrazka");
+			e.printStackTrace();
+		}	
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		Wyjscie wyjscie = new Wyjscie();
 		wyjscie.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -70,14 +68,26 @@ class MainPanel extends Panel{
 	void move(final JLayeredPane lp, final PlayPanel pp){		//przekazuje ale dziala
 		lp.moveToBack(this);
 		setVisible(false);
+		pp.repaint();
 		pp.setVisible(true);
+		
+	}
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawImage(tlo, 0, 0, width, height, null);
 	}
 }
 
-class PlayPanel extends Panel{
 
-	
+class PlayPanel extends Panel{
+	private BufferedImage tlo;
 	PlayPanel(){	
+		try {
+			tlo = ImageIO.read(new File("src/graphics/trawa.jpg"));
+		} catch (IOException e) {
+			System.err.println("Blad odczytu obrazka");
+			e.printStackTrace();
+		}	
 	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 	Wyjscie wyjscie = new Wyjscie();
 	wyjscie.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -96,5 +106,10 @@ class PlayPanel extends Panel{
 	add(wyjscie);
 	add(Box.createVerticalGlue());
 	//setOpaque(true);
-}
+	
+	}
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawImage(tlo, 0, 0, width, height, null);
+	}
 }
